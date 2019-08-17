74e1db345bae308af291463ecc4999a8c0a6b382
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-08-17 13:26:03 -0400

    added get routes for tables

--- server.js
+++ server.js
@@ -4,8 +4,42 @@ const server = express()
 
 server.use(express.json())
 
-
-
+server.get('/api/projects/', async (req, res) => {
+  try {
+    const projects = await db('projects')
+    res.status(200).json(projects)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'error retrieving projects table',
+      err
+    })
+  }
+})
+server.get('/api/tasks/', async (req, res) => {
+  try {
+    const tasks = await db('tasks').leftJoin('projects', 'projects.id', 'project_id')
+    res.status(200).json(tasks)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'error retrieving tasks table',
+      err
+    })
+  }
+})
+server.get('/api/resources/', async (req, res) => {
+  try {
+    const resources = await db('resources').leftJoin('projects', 'projects.id', 'project_id')
+    res.status(200).json(resources)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'error retrieving resources table',
+      err
+    })
+  }
+})
 
 
 module.exports = server
